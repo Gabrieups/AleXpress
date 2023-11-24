6de90ed79dbffa49b51869c5219d39ce803286eb
Gabrieups <paixaogabriel498@gmail.com>
2023-11-23 22:31:18 -0300

    ASDASDA

--- src/script.js
+++ src/script.js
@@ -7,12 +7,9 @@ function trackPackage() {
   const SVGstatus = document.getElementById('status');
 
   if (trackingNumber.trim() !== '') {
-    resultDiv.style.display = "block";
-    SVGstatus.style.opacity = 1;
-    SVGstatus.style.stroke = "grey";
+
   } else {
-    resultDiv.style.display = "block";
-    resultDiv.innerText = 'Por favor, insira um número de rastreamento.';
+
   }
 }
  trackerBtn.addEventListener('click', trackPackage);
